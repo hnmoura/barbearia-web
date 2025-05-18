120c04b4b2da2260091abce985ca6f4fc2b19bda
unknown <juliahlsantanaa@gmail.com>
2025-05-18 15:25:55 -0300

    Feat: Adicionado ao back

---
+++ src/views/admin/CadastroBarbeiro.jsx
@@ -0,0 +1,188 @@
+import { useState } from 'react';
+import { Form, Button, Grid, Icon  } from 'semantic-ui-react';
+import MenuSistema from '../../MenuSistema';
+import axios from "axios";
+
+//skills do barbeiro - futuramente vindo direto do banco
+const skillOptions = [
+    { key: 'barba', value: 'Barba', text: 'Barba' },
+    { key: 'corte_baixo', value: 'Corte Baixo', text: 'Corte Baixo' },
+    { key: 'corte_alto', value: 'Corte Alto', text: 'Corte Alto' },
+    { key: 'sobrancelha', value: 'Sobrancelha', text: 'Sobrancelha' },
+    { key: 'linha_corte', value: 'Linha de Corte', text: 'Linha de Corte' },
+];
+
+// Função para salvar os dados
+function CadastroBarbeiro() {
+  const [nome, setNome] = useState();
+  const [foneCelular, setFoneCelular] = useState();
+  const [email, setEmail] = useState();
+  const [dataNascimento, setDataNascimento] = useState();
+  const [cpf, setCpf] = useState();
+  const [endereco, setEndereco] = useState();
+  const [atendimentoInicio, setAtendimentoInicio] = useState();
+  const [atendimentoFim, setAtendimentoFim] = useState();
+  const [senha, setSenha] = useState();
+  const [skills, setSkills] = useState([]);
+
+
+
+
+  function salvar() {
+
+        let BarbeiroRequest = {
+            nome: nome,
+            foneCelular: foneCelular,
+            email: email,
+            dataNascimento: dataNascimento,
+            cpf: cpf,
+            endereco: endereco,
+            atendimentoInicio: atendimentoInicio,
+            atendimentoFim: atendimentoFim,
+            skills: skills.join(', '),
+            senha: senha
+        }
+    
+    //faz o post para o banco
+      axios.post("http://localhost:8080/api/barbeiro", BarbeiroRequest)
+    // exceção caso não funcione
+      .then((response) => {
+        console.log('barbeiro cadastrado com sucesso.')
+      })
+      .catch((error) => {
+        console.error('Erro ao incluir o barbeiro:', error.response?.data || error.message);
+      })
+    }
+
+  return (
+    <div className="ui container">
+      <MenuSistema tela={'barbeiro'} />
+      <Grid stackable centered>
+        <Grid.Row>
+          <Grid.Column mobile={16} tablet={8} computer={8}>
+            <Form className="ui form" widths='equal'>
+              <Form.Field>
+                <label>Nome Completo</label>
+                <input
+                  type="text"
+                  name="nome"
+                  value={nome}
+                        onChange={e => setNome(e.target.value)}
+                  placeholder="Nome do Cliente"
+                />
+              </Form.Field>
+              <Form.Field>
+                <label>Data de nascimento</label>
+                <input
+                  type="date"
+                  name="dataNascimento"
+                  value={dataNascimento}  // A data é mantida no formato yyyy-MM-dd
+                  onChange={e => setDataNascimento(e.target.value)}  // A data é salva diretamente no formato correto
+                />
+              </Form.Field>
+              <Form.Field>
+                <label>CPF</label>
+                <input
+                  type="text"
+                  name="cpf"
+                  value={cpf}
+                        onChange={e => setCpf(e.target.value)}
+                  placeholder="cpf"
+                />
+              </Form.Field>
+              <Form.Field>
+                <label>Endereço</label>
+                <input
+                    type="text"
+                    name="endereco"
+                    value={endereco}
+                    onChange={e => setEndereco(e.target.value)}
+                    placeholder="Endereço"
+                    />
+              </Form.Field>
+                <Form.Field>
+                    <label>Telefone</label>
+                    <input
+                    type="text"
+                    name="telefone"
+                    value={foneCelular}
+                    onChange={e => setFoneCelular(e.target.value)}
+                    placeholder="Número de telefone"
+                    />
+                </Form.Field>
+                <Form.Field>
+                <label>Email</label>
+                <input
+                  type="text"
+                  name="email"
+                  value={email}
+                onChange={e => setEmail(e.target.value)}
+                  placeholder="Email"
+                />
+                </Form.Field>
+                <Form.Field>
+                    <label>Começo do expediente</label>
+                    <input
+                    type="time"
+                    name="atendimentoInicio"
+                    value={atendimentoInicio}
+                    onChange={e => setAtendimentoInicio(e.target.value)}
+                    placeholder="Selecione o horário"
+                    />
+
+                    <label>Fim do expediente</label>
+                    <input
+                    type="time"
+                    name="atendimentoFim"
+                    value={atendimentoFim}
+                    onChange={e => setAtendimentoFim(e.target.value)}
+                    placeholder="Selecione o horário"
+                    />
+                </Form.Field>
+                <Form.Field>
+                <label>Skills do Barbeiro</label>
+                <Form.Dropdown
+                    placeholder='Selecione as skills'
+                    fluid
+                    multiple
+                    selection
+                    name="skills"
+                    options={skillOptions}
+                    value={skills}
+                    onChange={(e, { value }) => setSkills(value)}
+                />
+                </Form.Field>
+                <Form.Field>
+                <label>Senha</label>
+                <input
+                  type="text"
+                  name="senha"
+                  value={senha}
+                  onChange={e => setSenha(e.target.value)}
+                  placeholder="Escolha uma senha"
+                />
+              </Form.Field>
+
+              <Button
+                className="ui button"
+                inverted
+                circular
+                icon
+                labelPosition='left'
+                color='blue'
+                floated='right'
+                onClick={() => salvar()}
+              >
+                <Icon name='save' />
+                Salvar
+              </Button>
+
+            </Form>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </div>
+  );
+}
+
+export default CadastroBarbeiro;
