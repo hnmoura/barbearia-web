4a95073d415a3aaa2dfccd584cdfa8288f30039f
DORALICE FAGUNDES <cleibsonfsantos2@gmail.com>
2025-06-05 18:25:28 -0300

    Atualizando tela Home

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,17 +1,27 @@
 
 import React from "react";
-import { Container, Grid, Image, Step } from 'semantic-ui-react';
+import { Container, Grid, Image, Step,GridRow,GridColumn, Rating,Pagination } from 'semantic-ui-react';
 import MenuSistema from '../../MenuSistema';
 import '../../theme.css'; 
+
 
 
 
 function Home() {
   return (
     <div>
-      <MenuSistema tela={'home'} />
-      
+      <MenuSistema tela={'home'} />      
       <div style={{ marginTop: '5%' }}>
+
+        
+
+
+
+
+
+
+
+
         <Container>
           <Grid columns={2} divided>
             <Grid.Row>
@@ -53,6 +63,124 @@ function Home() {
             </Grid.Row>
           </Grid>
         </Container>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+
+          
+         <Grid celled='internally'>
+<GridRow>
+  <GridColumn width={3}>
+    <Image src='/barbearia.jpg' />
+  </GridColumn>
+  <GridColumn width={10}>
+    <Image src='/corte.jpg' />
+  </GridColumn>
+  <GridColumn width={3}>
+    <Image src='/corte2' />
+  </GridColumn>
+</GridRow>
+<GridRow>
+  <GridColumn width={3}>
+    <Image src='/corte3.jpg' />
+  </GridColumn>
+  <GridColumn width={10}>
+    <Image src='/images/wireframe/paragraph.png' />
+  </GridColumn>
+  <GridColumn width={3}>
+    <Image src='/images/wireframe/image.png' />
+  </GridColumn>
+</GridRow>
+</Grid>
+
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+
+
+
+    <div>
+
+    <h2/>Deixe sua avaliação pelo serviço Prestado<h2/>
+    <Rating maxRating={5} defaultRating={3} icon='star' size='mini' />
+    <br />
+    <br />
+
+    <Rating maxRating={5} defaultRating={3} icon='star' size='tiny' />
+    <br />
+    <br />
+
+    <Rating maxRating={5} defaultRating={3} icon='star' size='small' />
+    <br />
+    <br />
+
+    <Rating maxRating={5} defaultRating={3} icon='star' />
+    <br />
+    <br />
+
+    <Rating maxRating={5} defaultRating={3} icon='star' size='large' />
+    <br />
+    <br />
+
+    <Rating maxRating={5} defaultRating={3} icon='star' size='huge' />
+    <br />
+    <br />
+
+    <Rating maxRating={5} defaultRating={3} icon='star' size='massive' />
+    <br />
+    <br />
+  </div>
+
+
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<Pagination  style={{ display: 'flex', justifyContent: 'center' }}     defaultActivePage={5} totalPages={10} />
+
       </div>
     </div>
   );
